f90ce1bd11c3b11d53eec10945ae59e4f6f0cf8d
Mike Roberts <mikeroberts801@gmail.com>
2015-06-10 19:40:18 -0600

    Checkpoint 23 Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,5 @@
 require_relative "entry.rb"
-
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -19,5 +19,15 @@ class AddressBook
     end
     @entries.insert(index, Entry.new(name, phone, email))
   end
-  
+  def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true)
+ # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+ 
+     return csv.count
+   end
 end
